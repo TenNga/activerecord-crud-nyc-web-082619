c3dce39a2ea44adf3c9bcf40999e5d4840cd98b0
Kevin Karma <kevinkarma55@gmail.com>
2019-09-08 00:15:06 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -50,7 +50,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(id :1)
+  Movie.find_by(id:1)
 end
 
 def can_find_by_multiple_attributes
@@ -58,7 +58,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  
 end
 
 def can_find_using_where_clause_and_be_sorted
